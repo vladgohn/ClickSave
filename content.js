915354a5189473749636acf3fb6853d198400179
Vlad Gohn <vladgohn@gmail.com>
2023-12-03 18:58:41 +0200

    update

--- content.js
+++ content.js
@@ -1,14 +1,15 @@
 var extensionEnabled = false;
+var activeKey = 'Alt';
 
 function updateExtensionState() {
-  chrome.storage.local.get(['extensionEnabled'], function(result) {
+  chrome.storage.local.get(['extensionEnabled'], function (result) {
     extensionEnabled = result.extensionEnabled || false;
   });
 }
 
 updateExtensionState();
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
+chrome.storage.onChanged.addListener(function (changes, namespace) {
   for (var key in changes) {
     if (key === 'extensionEnabled') {
       extensionEnabled = changes[key].newValue;
@@ -16,47 +17,115 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
   }
 });
 
-document.addEventListener('click', function(e) {
-  if (!extensionEnabled) return;
-
-  // Функция для поиска элемента img, включая вложенные элементы
-  function findImgElement(element) {
-    if (element.tagName.toLowerCase() === 'img') {
-      return element;
-    }
-    if (element.querySelector) {
-      return element.querySelector('img');
-    }
-    return null;
+function findMediaElement(element) {
+  if (!element) return null;
+  
+  if (element.tagName.toLowerCase() === 'img' || element.tagName.toLowerCase() === 'video') {
+    return element;
   }
 
-  // Получаем элемент по координатам клика
+  // Поиск как изображения, так и видео
+  let imgElement = element.querySelector('img');
+  let videoElement = element.querySelector('video');
+  return imgElement || videoElement;
+}
+
+function addHighlightStyles() {
+  const style = document.createElement('style');
+  style.innerHTML = `
+    .highlighted-image {
+      // outline: 1px solid #ff62ab !important;
+      // box-shadow: inset 0 0 0px 3px rgba(0, 0, 255, 0.5) !important;
+      // z-index: 9995 !important;
+      // border: 1px solid lime !important;
+      // opacity: 0.5 !important;
+      // background: red !important;
+      cursor: cell !important;
+    }
+  `;
+  document.head.appendChild(style);
+}
+
+addHighlightStyles();
+
+document.addEventListener('click', function (e) {
+  if (!extensionEnabled || !e.getModifierState(activeKey)) return;
+
   let clickedElement = document.elementFromPoint(e.clientX, e.clientY);
-  let imgElement = findImgElement(clickedElement);
+  let mediaElement = findMediaElement(clickedElement);
 
-  // Если элемент не найден, пытаемся найти в родителях
-  while (!imgElement && clickedElement) {
+  while (!mediaElement && clickedElement) {
     clickedElement = clickedElement.parentElement;
-    imgElement = findImgElement(clickedElement);
+    mediaElement = findMediaElement(clickedElement);
   }
 
-  // Если изображение найдено, проверяем, был ли клик внутри его границ
-  if (imgElement) {
-    const imgRect = imgElement.getBoundingClientRect();
+  if (mediaElement) {
+    const mediaRect = mediaElement.getBoundingClientRect();
     const isInBounds =
-      e.clientX >= imgRect.left &&
-      e.clientX <= imgRect.right &&
-      e.clientY >= imgRect.top &&
-      e.clientY <= imgRect.bottom;
+      e.clientX >= mediaRect.left &&
+      e.clientX <= mediaRect.right &&
+      e.clientY >= mediaRect.top &&
+      e.clientY <= mediaRect.bottom;
 
     if (isInBounds) {
-      // Клик был сделан непосредственно по изображению
-      console.log('Изображение найдено:', imgElement.src);
-      window.open(imgElement.src, '_blank');
+      if (mediaElement.tagName.toLowerCase() === 'img') {
+        console.log('Изображение найдено:', mediaElement.src);
+        window.open(mediaElement.src, '_blank');
+      } else if (mediaElement.tagName.toLowerCase() === 'video') {
+        // Проверка наличия источника видео
+        let videoSrc = mediaElement.currentSrc || mediaElement.src;
+        if (videoSrc && videoSrc.startsWith('blob:')) {
+          console.log('Видео в формате Blob:', videoSrc);
+          // Здесь можно реализовать логику для обработки Blob URL
+        } else if (videoSrc) {
+          console.log('Видео найдено:', videoSrc);
+          window.open(videoSrc, '_blank');
+        } else {
+          console.log('Прямая ссылка на видео не найдена.');
+        }
+      }
     }
   } else {
-    console.log('Изображение не найдено.');
+    console.log('Медиа не найдено.');
   }
 }, true);
 
-  
+
+
+document.addEventListener('mouseover', function(e) {
+  if (!extensionEnabled) return;
+
+  let hoveredElement = document.elementFromPoint(e.clientX, e.clientY);
+  let imgElement = findMediaElement(hoveredElement);
+
+  if (imgElement) {
+    let targetElement = imgElement.parentElement; // Используем родительский элемент изображения
+    // Проверяем, не применен ли уже стиль к элементу
+    if (targetElement && !targetElement.classList.contains('highlighted-image')) {
+      targetElement.classList.add('highlighted-image');
+    }
+  }
+});
+
+document.addEventListener('mouseout', function(e) {
+  let highlightedElements = document.querySelectorAll('.highlighted-image');
+  highlightedElements.forEach(function(element) {
+    element.classList.remove('highlighted-image');
+  });
+});
+
+function downloadBlobVideo(blobUrl) {
+  fetch(blobUrl)
+    .then(response => response.blob())
+    .then(blob => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.style.display = 'none';
+      a.href = url;
+      a.download = 'video.mp4'; // Устанавливаем имя файла
+      document.body.appendChild(a);
+      a.click();
+      window.URL.revokeObjectURL(url);
+    })
+    .catch(e => console.error('Ошибка при скачивании видео:', e));
+}
